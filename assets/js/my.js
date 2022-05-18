3092b0f9f19b0050bbef621245fa8fc30dfa09d3
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2022-05-18 16:01:26 +0600

    slider section,compear img section style added

--- assets/js/my.js
+++ assets/js/my.js
@@ -2,3 +2,13 @@ $('.multiple-items').slick({
   slidesToShow: 3,
   slidesToScroll: 3
 });
+
+// compear img plugin
+
+$(function(){
+  $('.cpca-hero-right').beforeAfter();
+});
+
+$(function(){
+  $('.cpca-slider-cmpr-imgs').beforeAfter();
+});
