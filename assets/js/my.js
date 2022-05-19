0107952205c11ad52b5590f9786e192e9541c2de
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2022-05-19 23:14:27 +0600

    tablet size responsibe style added

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,9 +1,31 @@
-$('.multiple-items').slick({
+
+// slick slider section
+$('.cpca-slider-grid').slick({
+  infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 1,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 577,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
-// compear img plugin
+
+
+// compear img plugin section
 
 $(function(){
   $('.cpca-hero-right').beforeAfter();
