cca3e022858bb1d7ea78159e179eda6e86163779
azha18 <62115042+azha18@users.noreply.github.com>
2022-05-24 21:47:12 +0600

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+/**
+ * Gulp Config
+ * @version 1.0.0
+ * @author Md Azharul Islam <azhahiq786@gmail.com>
+ * @package ProjectBuilder
+ * @copyright 2020 WPFeel
+ */
+const app = require( './package.json' );
+const gulp = require( 'gulp' );
+const eslint = require( 'gulp-eslint' );
+const babel = require( 'gulp-babel' );
+const prettify = require( 'gulp-js-prettify' );
+const uglify = require( 'gulp-uglify' );
+const rename = require( 'gulp-rename' );
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require( 'gulp-sourcemaps' );
+const minifyCSS = require( 'gulp-clean-css' );
+const autoprefixer = require( 'gulp-autoprefixer' );
+const zip = require('gulp-zip');
+const notify = require( 'gulp-notify' );
+
+const config = {
+	babel: {
+		presets: ['@babel/preset-env']
+	},
+	prettify: {
+		"indent_with_tabs": true
+	},
+	js: {
+		src: ['assets/js/*.js', '!assets/js/*.min.js'],
+		dist: 'assets/js/',
+	},
+	scss: {
+		src: 'assets/css/*.scss',
+		dist: 'assets/css/',
+	},
+	autoprefixer: {
+		options: {
+			cascade: false,
+		},
+	},
+};
+
+// Tasks
+gulp.task(
+	'compile:js',
+	() => {
+		return gulp.src( config.js.src )
+			.pipe( sourcemaps.init( { largeFile: true } ) )
+			.pipe( eslint() )
+			.pipe( eslint.format() )
+			.pipe( babel( config.babel ) )
+			.on( 'error', notify.onError( {title: "Error", message: "Error: <%= error.message %>"} ) ) // phpcs:ignore WordPressVIPMinimum.Security.Underscorejs.OutputNotation
+			.pipe( prettify( config.prettify ) )
+			// .pipe( gulp.dest( config.js.dist ) )
+			.pipe( uglify() )
+			.pipe( rename( { suffix: '.min' } ) )
+			.pipe( sourcemaps.write( '/.' ) )
+			.pipe( gulp.dest( config.js.dist ) )
+			.pipe( notify( {message: 'TASK: compile:js Completed! 💯', onLast: true} ) );
+	}
+);
+gulp.task(
+	'compile:scss',
+	() => {
+		return gulp.src( config.scss.src )
+			.pipe( sass().on( 'error', sass.logError ) )
+			.on( 'error', notify.onError( {title: "Error", message: "Error: <%= error.message %>"} ) ) // phpcs:ignore WordPressVIPMinimum.Security.Underscorejs.OutputNotation
+			.pipe( autoprefixer( config.autoprefixer.options ) )
+			.pipe( gulp.dest( config.scss.dist ) )
+			.pipe( minifyCSS() )
+			.pipe( rename( { suffix: '.min'} ) )
+			.pipe( gulp.dest( config.scss.dist ) )
+			.pipe( notify( {message: 'TASK: compile:scss Completed! 💯', onLast: true} ) );
+	}
+);
+gulp.task(
+	'compile:public:scss',
+	() => {
+		return gulp.src( config.public_scss.src )
+			.pipe( sass().on( 'error', sass.logError ) )
+			.on( 'error', notify.onError( {title: "Error", message: "Error: <%= error.message %>"} ) ) // phpcs:ignore WordPressVIPMinimum.Security.Underscorejs.OutputNotation
+			.pipe( autoprefixer( config.autoprefixer.options ) )
+			.pipe( gulp.dest( config.public_scss.dist ) )
+			.pipe( minifyCSS() )
+			.pipe( rename( { suffix: '.min'} ) )
+			.pipe( gulp.dest( config.public_scss.dist ) )
+			.pipe( notify( {message: 'TASK: compile:scss Completed! 💯', onLast: true} ) );
+	}
+);
+gulp.task(
+	'compile:public:js',
+	() => {
+		return gulp.src( config.public_js.src )
+			.pipe( sourcemaps.init( { largeFile: true } ) )
+			.pipe( eslint() )
+			.pipe( eslint.format() )
+			.pipe( babel( config.babel ) )
+			.on( 'error', notify.onError( {title: "Error", message: "Error: <%= error.message %>"} ) ) // phpcs:ignore WordPressVIPMinimum.Security.Underscorejs.OutputNotation
+			.pipe( prettify( config.prettify ) )
+			// .pipe( gulp.dest( config.js.dist ) )
+			.pipe( uglify() )
+			.pipe( rename( { suffix: '.min' } ) )
+			.pipe( sourcemaps.write( '/.' ) )
+			.pipe( gulp.dest( config.public_js.dist ) )
+			.pipe( notify( {message: 'TASK: compile:js Completed! 💯', onLast: true} ) );
+	}
+);
+gulp.task( 'build', gulp.series( 'compile:js', 'compile:scss' ) );
+gulp.task(
+	'watch',
+	function () {
+		gulp.watch( config.js.src, gulp.series( 'compile:js' ) );
+		gulp.watch( config.scss.src, gulp.series( 'compile:scss' ) );
+	}
+);
+
